3d58415fbb2926538bdd865f13b9c982ce82266c
fanlumaster <1730976608@qq.com>
2023-09-16 15:11:54 +0800

    First commit.
    Then, I love Niya.
    
    Then, Cause I fully broke the commit before, so, this is first
    commit.

---
+++ README.md
@@ -0,0 +1,10 @@
+# About
+
+99.99% inspired by [AstroNvim](https://github.com/AstroNvim).
+
+Based on AstroNvim, I made my own customized config by my own preference. Some of these configs are not that elegant, cause that now I don't have so much time to apply so said best practice.
+
+But I also believe that someone would get it easy to configure some parts of neovim or AstroNvim when watching my config, to some extend. For example, I found that many guys do not exactly know how to change cursor color of neovide, a gui front-end app for neovim, I also did not know before, but eventually I make it. I think that's it and would make some little sense. For another example, that's hop-nvim, and I make it more like easymotion in vim and vscode-vim.
+
+
+
---
+++ lua/user/mappings.lua
@@ -0,0 +1,85 @@
+-- Mapping data with "desc" stored directly by vim.keymap.set().
+--
+-- Please use this mappings table to set keyboard mapping since this is the
+-- lower level configuration and more robust one. (which-key will
+-- automatically pick-up stored data by this setting.)
+return {
+  -- first key is the mode
+  n = {
+    -- second key is the lefthand side of the map
+
+    -- navigate buffer tabs with `H` and `L`
+    L = {
+      function() require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end,
+      desc = "Next buffer",
+    },
+    H = {
+      function() require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
+      desc = "Previous buffer",
+    },
+
+    -- mappings seen under group name "Buffer"
+    ["<leader>bD"] = {
+      function()
+        require("astronvim.utils.status").heirline.buffer_picker(
+          function(bufnr) require("astronvim.utils.buffer").close(bufnr) end
+        )
+      end,
+      desc = "Pick to close",
+    },
+    -- tables with the `name` key will be registered with which-key if it's installed
+    -- this is useful for naming menus
+    ["<leader>b"] = { name = "Buffers" },
+    -- quick save
+    -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+
+    -- for hop.nvim
+    ["<leader><leader>w"] = {
+      function()
+        vim.cmd('.g/^$/normal )')
+        require'hop'.hint_words({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR })
+      end,
+    },
+
+    ["<leader><leader>e"] = {
+      function()
+        vim.cmd('.g/^$/normal )')
+        require'hop'.hint_words({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, hint_position = require'hop.hint'.HintPosition.END })
+      end,
+    },
+
+    ["<leader><leader>b"] = {
+      function()
+        vim.cmd('.g/^$/normal b')
+        require'hop'.hint_words({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR })
+      end,
+    },
+
+    ["<leader><leader>a"] = {
+      function()
+        require'hop'.hint_anywhere()
+      end,
+    },
+
+    ["<leader><leader>j"] = {
+      function()
+        vim.cmd('.g/^$/normal )')
+        require'hop'.hint_lines({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR })
+      end,
+    },
+
+    ["<leader><leader>k"] = {
+      function()
+        vim.cmd('.g/^$/normal b')
+        require'hop'.hint_lines({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR })
+      end,
+    },
+
+    ["<leader>py"] = { "<cmd>TermExec cmd=\"python %\"<cr>", desc = "Run python codes" },
+
+  },
+  t = {
+    -- setting a mapping to false will disable it
+    -- ["<esc>"] = false,
+  },
+}
